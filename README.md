3e0247e18283be3f226f042fe2cd1c5efc71519e
Nicole Martin <n.martinpdx@gmail.com>
2021-10-22 12:26:43 -0700

    playing around with inline and config styling

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Tailwind_sandbox
 Learning Tailwind CSS
+
+Tutorial from *Traversy Media* 
+[Tailwind CSS Crash Course](https://www.youtube.com/watch?v=UBOj6rqRUME)
+
+
+- It is a utility-first, low-level CSS Framework.
+- No predefined components, more customization, less opinionated 
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      colors: {
+        yellow: {
+          DEFAULT: '#FDE68A',
+
+        }
+      },
+      center: true,
+      padding: '2rem'
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
